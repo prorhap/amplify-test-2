cddc07c3d745867f6203389d7947cd6062faf2d1
prorhap <choichul+demo@amazon.com>
2024-10-04 19:58:20 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import "./App.css";
 
-function App() {
+const client = generateClient<Schema>();
+
+export default function App() {
+  const [memos, setMemos] = useState<Schema["Memo"]["type"][]>([]);
+
+  const fetchMemos = async () => {
+    const { data: items, errors } = await client.models.Memo.list();
+    if (!errors) {
+      setMemos(items);
+    }
+  };
+
+  useEffect(() => {
+    fetchMemos();
+  }, []);
+
+  const createMemo = async () => {
+    const content = window.prompt("Memo");
+    if (content) {
+      await client.models.Memo.create({
+        content
+      });
+      fetchMemos();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button className="button" onClick={createMemo}>Add new memo</button>
+      <ul className="list">
+        {memos.map(({ id, content }) => (
+          <li className="list-item" key={id}>
+            {content}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
